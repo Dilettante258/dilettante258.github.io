700a0ba69874060615b512298640c54b8837e604
Dilettante258 <2226519330@qq.com>
2023-12-24 09:15:12 +0800

    change domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dilettante258.github.io',
+  site: 'https://wang1m.cc',
   base: '/',
 })
